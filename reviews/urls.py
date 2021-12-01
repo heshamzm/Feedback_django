aa51405217a23a64bc98724e4c45234d9578f837
Heshamzm <heshammaree@icloud.com>
2021-12-01 17:48:00 +0200

    new template added

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # add urls and path for each view
 urlpatterns = [
-    path("",views.review)
+    path("",views.review),
+    path("thank-you", views.thank_you)
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+import reviews
 
 # Create your views here.
 def review(request):
-    return render (request, "reviews/review.html")
+    if request.method == "POST":
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+    
+    return render(request, "reviews/review.html")
+
+
+def thank_you(requset):
+
+    return render(requset, "reviews/thank_you.html" )
