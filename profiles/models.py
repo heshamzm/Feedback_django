ac0cd795c47a395b3a5380deffcb17e3f535f698
Heshamzm <heshammaree@icloud.com>
2021-12-02 18:53:30 +0200

    created a model to save the uploaded files in the a folder

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    image = models.FileField(upload_to="images") # ulooad_to create a file inside the file we added in the settings.
