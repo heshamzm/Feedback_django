2d00a967422e09af1db23749659f91dd120d6a32
Heshamzm <heshammaree@icloud.com>
2021-12-02 22:10:46 +0200

    adding image field

--- profiles/models.py
+++ profiles/models.py
@@ -4,4 +4,8 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="images") # ulooad_to create a file inside the file we added in the settings.
+    #image = models.FileField(upload_to="images") # ulooad_to create a file inside the file we added in the settings.
+    image = models.ImageField(upload_to="images")
+
+    def __str__(self) :
+        return f"{self.image}"
